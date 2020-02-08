4ee72f9be0c32d589eb9190eae2636f5648a9e06
sohadQtaitat <qtaitat3@gmail.com>
2020-02-08 16:09:13 +0200

    added middleware test

---
+++ __test__/middleware.test.js
@@ -0,0 +1,28 @@
+    'use strict';
+    
+    const { server } = require('../lib/server.js');
+    const supertest = require('supertest');
+    const mockRequest = supertest(server);
+    
+    describe('api server', () => {
+        
+        
+        it('test error 500  ', () => {
+            return mockRequest
+            .get('/gen-error')
+            .then(data => {
+                expect(data.status).toBe(500);
+            //expect(response.status).toEqual(500);
+            }).catch(e => console.error(e));
+        });  
+        
+     
+        it('test not found route 404', () => {
+            return mockRequest
+            .get('/main')
+      .then(data => {
+        expect(data.status).toBe(404);
+      }).catch(e => console.error(e));
+  }); 
+
+});
